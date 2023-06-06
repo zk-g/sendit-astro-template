29d4204a6ace3e28b953ebad2f1746d832749e30
Giulio Zanchetta <zank@outlook.it>
2023-06-06 21:55:32 +0200

    Add netlify cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,38 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-import bookshop from '@bookshop/astro-bookshop';
+import bookshop from "@bookshop/astro-bookshop";
+import NetlifyCMS from "astro-netlify-cms";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://top-quail.cloudvent.net/",
-  integrations: [bookshop(), react()]
+	site: "https://top-quail.cloudvent.net/",
+	integrations: [
+		bookshop(),
+		react(),
+		NetlifyCMS({
+			config: {
+				backend: {
+					name: "github",
+					repo: "zk-g/sendit-astro-template",
+					branch: "main",
+					base_url: "https://sendit-astro-template.pages.dev",
+					auth_endpoint: "/api/auth",
+				},
+				collections: [
+					// Content collections
+					{
+						name: "posts",
+						label: "Blog Posts",
+						folder: "src/content/blog",
+						create: true,
+						delete: true,
+						fields: [
+							{ name: "title", widget: "string", label: "Post Title" },
+							{ name: "body", widget: "markdown", label: "Post Body" },
+						],
+					},
+				],
+			},
+		}),
+	],
 });
