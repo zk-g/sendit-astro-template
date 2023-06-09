058c3051ff74784848eb2ad6aebaa4da598fa5ce
Giulio Zanchetta <zank@outlook.it>
2023-06-09 22:40:50 +0000

    add client id and token to tina cms config

--- tina/config.ts
+++ tina/config.ts
@@ -4,42 +4,42 @@ import { defineConfig } from "tinacms";
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
-  branch,
-  clientId: null, // Get this from tina.io
-  token: null, // Get this from tina.io
+	branch,
+	clientId: process.env.TINA_CLIENT_ID ?? null, // Get this from tina.io
+	token: process.env.TINA_TOKEN ?? null, // Get this from tina.io
 
-  build: {
-    outputFolder: "admin",
-    publicFolder: "public",
-  },
-  media: {
-    tina: {
-      mediaRoot: "",
-      publicFolder: "public",
-    },
-  },
-  schema: {
-    collections: [
-      {
-        name: "post",
-        label: "Posts",
-        path: "content/posts",
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-        ],
-      },
-    ],
-  },
+	build: {
+		outputFolder: "admin",
+		publicFolder: "public",
+	},
+	media: {
+		tina: {
+			mediaRoot: "",
+			publicFolder: "public",
+		},
+	},
+	schema: {
+		collections: [
+			{
+				name: "post",
+				label: "Posts",
+				path: "content/posts",
+				fields: [
+					{
+						type: "string",
+						name: "title",
+						label: "Title",
+						isTitle: true,
+						required: true,
+					},
+					{
+						type: "rich-text",
+						name: "body",
+						label: "Body",
+						isBody: true,
+					},
+				],
+			},
+		],
+	},
 });
