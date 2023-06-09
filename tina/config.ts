885323c6135ba1c92cb619106d0a060f969c8493
Giulio Zanchetta <zank@outlook.it>
2023-06-09 23:05:22 +0000

    Fix tina collections

--- tina/config.ts
+++ tina/config.ts
@@ -23,7 +23,27 @@ export default defineConfig({
 			{
 				name: "post",
 				label: "Posts",
-				path: "content/posts",
+				path: "src/content/blog",
+				fields: [
+					{
+						type: "string",
+						name: "title",
+						label: "Title",
+						isTitle: true,
+						required: true,
+					},
+					{
+						type: "rich-text",
+						name: "body",
+						label: "Body",
+						isBody: true,
+					},
+				],
+			},
+			{
+				name: "pages",
+				label: "Pages",
+				path: "src/content/pages",
 				fields: [
 					{
 						type: "string",
