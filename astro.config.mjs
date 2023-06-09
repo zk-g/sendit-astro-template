53d998fe815c63e535af7bf66dc14a26ce46b396
Giulio Zanchetta <zank@outlook.it>
2023-06-09 21:56:56 +0000

    Remove astro-netlify-cms from config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,39 +1,9 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import bookshop from "@bookshop/astro-bookshop";
-// https://github.com/i40west/netlify-cms-cloudflare-pages
-import NetlifyCMS from "astro-netlify-cms";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://sendit-astro-template.pages.dev",
-	integrations: [
-		// NetlifyCMS({
-		// 	config: {
-		// 		backend: {
-		// 			name: "github",
-		// 			repo: "zk-g/sendit-astro-template",
-		// 			branch: "main",
-		// 			base_url: "https://sendit-astro-template.pages.dev",
-		// 			auth_endpoint: "/api/auth",
-		// 		},
-		// 		collections: [
-		// 			// Content collections
-		// 			{
-		// 				name: "posts",
-		// 				label: "Blog Posts",
-		// 				folder: "src/content/blog",
-		// 				create: true,
-		// 				delete: true,
-		// 				fields: [
-		// 					{ name: "title", widget: "string", label: "Post Title" },
-		// 					{ name: "body", widget: "markdown", label: "Post Body" },
-		// 				],
-		// 			},
-		// 		],
-		// 	},
-		// }),
-		bookshop(),
-		react(),
-	],
+	integrations: [bookshop(), react()],
 });
