4ff6bed7ecf665cd2cdee6e1046f56e2445d7f1e
Giulio Zanchetta <zank@outlook.it>
2023-06-10 10:09:19 +0200

    Add @astrojs/image and configure it in astro.config.mjs
    This commit adds the "@astrojs/image" dependency to package.json and configures it in astro.config.mjs. The image integration is added to the integrations array with a serviceEntryPoint of "@astrojs/image/sharp" and a cacheDir of "./.cache/image".

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,17 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import bookshop from "@bookshop/astro-bookshop";
 
+import image from "@astrojs/image";
+
 // https://astro.build/config
 export default defineConfig({
 	site: "https://sendit-astro-template.pages.dev",
-	integrations: [bookshop(), react()],
+	integrations: [
+		bookshop(),
+		react(),
+		image({
+			serviceEntryPoint: "@astrojs/image/sharp",
+			cacheDir: "./.cache/image"
+		}),
+	],
 });
